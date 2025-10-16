e963d1cd6c986c589fd0887b8db4dbb107793b40
yongsulim <sulim.yong88@gmail.com>
2025-10-16 21:32:11 +0900

    웹 구성

---
+++ android/app/src/main/java/com/teto/egen/test/MainActivity.java
@@ -0,0 +1,5 @@
+package com.teto.egen.test;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.teto.egen.test',
+  appName: 'teto-egen-test',
+  webDir: 'build'
+};
+
+export default config;
